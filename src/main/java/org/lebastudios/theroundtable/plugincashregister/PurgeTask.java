dcfbb69848e7436ad4c91d72601a6a5522b23401
Borja Castellano <borjacastellano1@gmail.com>
2025-04-13 13:58:40 +0200

    purge task class created but not implemented

---
+++ src/main/java/org/lebastudios/theroundtable/plugincashregister/PurgeTask.java
@@ -0,0 +1,13 @@
+package org.lebastudios.theroundtable.plugincashregister;
+
+import org.lebastudios.theroundtable.plugins.IPlugin;
+import org.lebastudios.theroundtable.tasks.Task;
+
+public class PurgeTask extends Task<IPlugin.PurgeTaskResult>
+{
+    @Override
+    protected IPlugin.PurgeTaskResult call() throws Exception
+    {
+        return null;
+    }
+}
